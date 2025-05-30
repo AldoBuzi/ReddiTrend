55047238f5c884f1e3a6e7ee388be88696f20b94
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2025-05-30 12:05:57 +0200

    Added schema initialization

--- cassandra/setup.sh
+++ cassandra/setup.sh
@@ -2,3 +2,9 @@
 
 # Deploy Cassandra to the cluster
 kubectl apply -f cassandra.yaml -n kafka
+
+# Ceate the ConfigMap for the schema
+kubectl create configmap cassandra-schema --from-file=schema.cql -n kafka
+
+# Deploy the schema initialization
+kubectl apply -f cassandra-init.yaml
